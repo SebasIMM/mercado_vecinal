94045a3b8f2c84a2c6afecef151489e348a750e9
Sebastian Medel <sevastian.ignis@gmail.com>
2023-11-22 12:49:39 -0300

    Recreated CORS and optimized handling of static paths using join in ESM

--- server.js
+++ server.js
@@ -1,51 +1,40 @@
-import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
-import expressLayouts from'express-ejs-layouts';
-// todo: añadir path para corregir las rutas
-
-const app = express()
-const port = process.env.PORT || 3000
-
-// static routes
-app.use(express.static('./public'));
-app.use(express.static('./node_modules/bootstrap/dist'));
-app.use(express.static('./node_modules/axios/dist'));
+import expressLayouts from 'express-ejs-layouts';
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
 
 // routes
-import indexRouter from './src/routes/index.routes.js' // webpage routes
-import productsRouter from './src/routes/products.routes.js'
+import indexRouter from './src/routes/index.routes.js'; // webpage routes
+import productsRouter from './src/routes/products.routes.js';
+import {corsMiddleware} from './src/middlewares/cors.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// static routes
+app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+app.use(express.static(join(__dirname, 'node_modules', 'axios', 'dist')));
 
 // middlewares
 app.disable('x-powered-by');
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(expressLayouts);
-app.use(cors({
-    origin: (origin, callback) => {
-        const ACCEPTED_ORIGINS = [
-            'http://localhost:8080'
-        ]
-
-        if (ACCEPTED_ORIGINS.includes(origin)) return callback(null, true);
-        if (!origin) return callback(null, true);
-
-        return callback(new Error('Not allowed by CORS'))
-    }
-}));
+app.use(corsMiddleware());
 
 // view engine
-app.set('view engine', 'ejs')
-app.set('layout','layouts/main') // layout route from 'views', route like 'folfer/file'
-app.set('views', './src/views/'); // views route from this point
+app.set('view engine', 'ejs');
+app.set('layout', join(__dirname, 'src', 'views', 'layouts', 'main')); // layout default route
+app.set('views', join(__dirname, 'src', 'views')); // all views route
 
 // endpoints
-app.use('/', indexRouter)
-app.use('/api/products', productsRouter)
+app.use('/', indexRouter);
+app.use('/api/products', productsRouter);
 
-// ruta de error // todo falta mejorarla, enviarla al final de indexRouter
-app.use((req, res) => {
-    res.status(404).send('<h1>Esta pagina no existe</h1>')
-})
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
