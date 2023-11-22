5d975732c3f46e1d1580ba6ffe57ef9df89715fa
Sebastian Medel <sevastian.ignis@gmail.com>
2023-11-22 12:41:05 -0300

    Trying to fix errors when uploading files01~

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -16,4 +16,9 @@ router.get("/contact", function (req, res) {
     res.render('./pages/contact', {title:'Contacto'})
 })
 
+// Handling Not Found Routes (404)
+router.use((req, res) => {
+    res.render('./pages/error', {title:'404'})
+})
+
 export default router;
